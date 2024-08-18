2fffd771a76fada3b8d13e6aa3270819a2e2674e
YASH2204V <yash.v220104@gmail.com>
2024-08-18 22:50:43 +0530

    added separation of concern in models and make router then API testing with Postman

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const ejs = require("ejs");
 const path = require('path');
 
-const userModel = require('./models/user-model');
-const productModel = require('./models/product-model');
+const usersRouter = require('./routes/usersRouter');
+const productsRouter = require('./routes/productsRouter');
+const ownersRouter = require('./routes/ownersRouter');
 
+const db = require('./config/mongoose-connection');
 const cookieParser = require("cookie-parser");
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,9 +16,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("view engine", "ejs");
 
-app.get("/", (req,res)=>{
-    res.send("Hello World!");
-})
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+app.use('/owners', ownersRouter);
+
 
 app.listen(3000, ()=>{
     console.log("Server running on http://localhost:3000");
---
+++ models/owner-model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const ownerSchema = mongoose.Schema({
+    fullname: String,
+    email: String,
+    password: String,
+
+    products: {
+        type: Array,
+        default: []
+    },
+    contact: Number,
+    picture: String,
+    gstin: String
+});
+
+module.exports = mongoose.model("owner", ownerSchema);
